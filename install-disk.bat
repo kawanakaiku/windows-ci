2ec1f3a73960c4c884687b2b2db5f567518e1343
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-07-13 00:45:06 +0900

    Create install-disk.bat

---
+++ install-disk.bat
@@ -0,0 +1,28 @@
+::run as admin
+if defined SESSIONNAME (PowerShell start """%~0""" -verb RunAs & Exit /B)
+
+echo list disk | diskpart
+echo input disk number
+set input_int=
+
+(
+  echo SELECT DISK 0
+  echo CLEAN
+  echo CONVERT GPT
+  echo CREATE PARTITION EFI SIZE=100
+  echo FORMAT QUICK FS=FAT32
+  echo ASSIGN LETTER="S"
+  echo CREATE PARTITION MSR SIZE=16
+  echo CREATE PARTITION PRIMARY
+  echo SHRINK MINIMUM=500
+  echo FORMAT QUICK FS=NTFS
+  echo ASSIGN LETTER="W"
+  echo CREATE PARTITION PRIMARY
+  echo FORMAT QUICK FS=NTFS
+) | diskpart
+
+DISM /Apply-Image /ImageFile:D:\Sources\install.wim /index:1 /ApplyDir:W:\
+
+DISM /Image:W:\ /Set-LayeredDriver:6
+
+BCDBOOT W:\Windows /l ja-jp /s S: /f UEFI
